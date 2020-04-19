009830d6ad3e5e8a75294566fcf7494a8c934c5c
Ilias <igoujgali@gmail.com>
2020-04-19 12:25:24 +0200

    Create monopolygame

--- src/main/java/monopoly/Player.java
+++ src/main/java/monopoly/Player.java
@@ -1,32 +1,48 @@
 package monopoly;
 
+import java.util.List;
+
 public class Player {
 
     private String name;
     private Piece piece;
-    private Die die;
+    private List<Die> dice;
     private Board board;
 
 
-    public Player(String name, Piece piece){
-        this.name = name;
-        this.piece = piece;
+    public Player(int id, List<Die> dice, Board board){
+        this.name = "Player"+id;
+        this.piece =  new Piece( id, board.getInitalLocation());
+        this.dice = dice;
+        this.board = board;
     }
 
     /**
      * @brief : player takes turn : rolls dice, finds new location for its piece and moves it to the right destination
      * */
     public void takeTurn(){
+        int total=0;
         // roll dice twice
-        int faceValue1 = die.getFaceValue();
-        int faceValue2 = die.getFaceValue();
+        for(Die die : dice){
+            die.roll();
+            total += die.getFaceValue();
+        }
+        System.out.println(String.format("%s est sur %s", this, piece.getCurrentPosition()));
+        System.out.println(String.format("%s lance le dé et fait %d", this, total));
+
         // calculate new square destination
-        Square destination = board.getLocation(piece.getCurrentPosition() , faceValue1 + faceValue2);
+        Square destination = board.getLocation(piece.getCurrentPosition(), total);
+
+
         // set piece to new square position
         piece.move(destination);
+        System.out.println(String.format("%s se déplace sur %s", this, piece.getCurrentPosition()));
     }
 
-
+    @Override
+    public String toString() {
+        return name;
+    }
 }
 
 
