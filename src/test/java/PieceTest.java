21c5d5f5525b64325c3984fb5a45973815a4774f
RosalieChhen <mei-heang.chhen@heig-vd.ch>
2020-04-19 10:20:51 +0200

    draft Piece class and tests added

---
+++ src/test/java/PieceTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+    @Test
+    public void itShouldWork() {
+        assertEquals(true, true);
+    }
+
+}
